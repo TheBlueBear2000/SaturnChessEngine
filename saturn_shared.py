2df8293fe73ba45d6bdf5e2cf660b4133e5a95bb
George Berkeley <gberkeley1@sheffield.ac.uk>
2026-07-29 10:45:08 +0100

    Split move stripping logic and began legality validation

--- saturn_shared.py
+++ saturn_shared.py
@@ -1,5 +1,6 @@
 CHANNEL_ORDER = ["R", "N", "B", "Q", "K"]
 CHANNEL_VALUES = [1, 5, 3, 3, 10]  # Includes pawn at start, excludes king at end
+from math import log2
 
 
 class Board:
@@ -27,27 +28,36 @@ class Board:
         piece = move.pop(0)
         return (CHANNEL_ORDER.index(piece) + 1) + (6 * int(whiteMove)), move
 
-    def squareToInt(
-        self, square
-    ):  # Assumes a form of letter number, such as a3, c7, etc
-        return ord(square[0].lower()) + (8 * (int(square[1]) - 1))
+    def squareToInt(self, square: str):
+        # Assumes a form of letter number, such as a3, c7, etc
+        return (ord(square[0].lower()) - 97) + (8 * (int(square[1]) - 1))
 
-    def positionMask(self, square):
+    def intToSquare(self, position: int):
+        return chr((position % 8) + 97) + str(position // 8)
+
+    def getMaskPositions(self, mask: int):
+        positions = []
+        for i in range(64):
+            testMask = 1 << i
+            if testMask & mask:
+                positions.append(self.intToSquare(i))
+
+    def positionMask(self, square: str):
         return 1 << self.squareToInt(square)
 
-    def movePiece(self, startPos, endPos, channel):
+    def movePiece(self, startPos: str, endPos: str, channel: int):
         startMask = self.positionMask(startPos)
         endMask = self.positionMask(endPos)
         self.state[channel] &= ~startMask  # Remove piece from start pos
         self.state[channel] |= endMask  # Add piece back to end pos
 
-    def getPieceValue(self, channel):
+    def getPieceValue(self, channel: int):
         modular_channel = channel % 6
         if modular_channel == 5:
             raise Exception("Cannot get king value")
         return CHANNEL_VALUES[modular_channel]
 
-    def takePiece(self, takePos, whiteMove):
+    def takePiece(self, takePos: str, whiteMove: bool):
         takeMask = self.positionMask(takePos)
         for c, channel in enumerate(self.state):
             if takeMask & channel:
@@ -62,12 +72,8 @@ class Board:
             f"Move alledges to capture piece, but there is no piece to capture at {endPos}"
         )
 
-    def get_board(self):
-        return self.state
+    def stripMove(self, move: str, whiteMove: bool):
 
-    # def moveLegal(move: str) # Checks move legality
-
-    def update(self, move: str, whiteMove: bool):  # Assumes all moves are legal
         # Remove check and checkmate notation
         move = move.strip("#")
         move = move.strip("+")
@@ -88,25 +94,16 @@ class Board:
             promoteTo = move[1]
         move = move[0]
 
-        # Castling
-        if move.upper() == "O-O-O":  # Long castle
-            if whiteMove:
-                self.movePiece("e1", "c1", 5)  # Move king
-                self.movePiece("a1", "d1", 1)  # Move rook
-            else:
-                self.movePiece("e8", "c8", 5)  # Move king
-                self.movePiece("a8", "d8", 1)  # Move rook
-            return
-        elif move.upper() == "O-O":  # Short castle
-            if whiteMove:
-                self.movePiece("e1", "g1", 5)  # Move king
-                self.movePiece("h1", "f1", 1)  # Move rook
-            else:
-                self.movePiece("e8", "c8", 5)  # Move king
-                self.movePiece("a8", "d8", 1)  # Move rook
-            return
+        longCastle = move.upper() == "O-O-O"
+        shortCastle = move.upper() == "O-O"
+
+        if longCastle or shortCastle:
+            return "", "", 0, enpassant, capture, promoteTo, longCastle, shortCastle
 
         # If move is not castling, it follows standard form
+        # Exctract moved piece channel
+        pieceChannel, move = self.identifyChannelFromMove(move, whiteMove)
+
         # Split move into beginning and end positions
         positions = move.split("-")  # Split into beginning and end pos
         startPos, endPos = positions[0], positions[1]
@@ -123,10 +120,49 @@ class Board:
         ):
             raise Exception(f"End Pos is invalid: {endPos}")
 
-        # After castling, get piece move
-        pieceChannel, move = self.identifyChannelFromMove(
-            move, whiteMove
-        )  # Remove and store piece type
+        return (
+            startPos,
+            endPos,
+            pieceChannel,
+            enpassant,
+            capture,
+            promoteTo,
+            longCastle,
+            shortCastle,
+        )
+
+    def get_board(self):
+        return self.state
+
+    def update(self, move: str, whiteMove: bool):  # Assumes all moves are legal
+        (
+            startPos,
+            endPos,
+            pieceChannel,
+            enpassant,
+            capture,
+            promoteTo,
+            longCastle,
+            shortCastle,
+        ) = self.stripMove(move, whiteMove)
+
+        # Castling
+        if longCastle:  # Long castle
+            if whiteMove:
+                self.movePiece("e1", "c1", 5)  # Move king
+                self.movePiece("a1", "d1", 1)  # Move rook
+            else:
+                self.movePiece("e8", "c8", 5)  # Move king
+                self.movePiece("a8", "d8", 1)  # Move rook
+            return
+        elif shortCastle:  # Short castle
+            if whiteMove:
+                self.movePiece("e1", "g1", 5)  # Move king
+                self.movePiece("h1", "f1", 1)  # Move rook
+            else:
+                self.movePiece("e8", "c8", 5)  # Move king
+                self.movePiece("a8", "d8", 1)  # Move rook
+            return
 
         # Capture
         if capture:
@@ -150,3 +186,31 @@ class Board:
             ] |= self.positionMask(
                 endPos
             )  # Add new piece by channel
+
+    def isCheck(self, isWhiteCheck, board=self.state):
+        if isWhiteCheck:
+            attackingPieces = board[6:]
+            # Use the fact that there is 1 king and so location must be power of two to identify digit position in mask
+            kingPos = intToSquare(log2(board[5]))
+        else:
+            attackingPieces = board[:6]
+            kingPos = intToSquare(log2(board[11]))
+        
+        for pawn in getMaskPositions(attackingPieces[0]):
+            
+
+    def moveLegal(move: str, whiteMove: bool):  # Checks move legality
+        (
+            startPos,
+            endPos,
+            pieceChannel,
+            enpassant,
+            capture,
+            promoteTo,
+            longCastle,
+            shortCastle,
+        ) = self.stripMove(move, whiteMove)
+
+        # Check castling legality
+        if shortCastle:
+            {}
